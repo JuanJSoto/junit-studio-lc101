377c3db87ce34ae375067c7a4bde1aa042fd5a9e
JuanJSoto <JJ3185@hotmail.com>
2022-08-07 19:07:50 -0400

    6.4 studio for 8-4-22 unit testing

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,10 +1,14 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+    final String test_brackets = "[[[[[[[";
+
+
 
     //TODO: add tests here
     @Test
@@ -12,5 +16,38 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+//    @Test
+//    public void onlyBracketsIsNull(){
+//        assertFalse(BalancedBrackets.hasBalancedBrackets(null));
+//    }
 
+    @Test
+    public void onlyBracketsHasAllLetters(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+    @Test
+    public void onlyBracketsHasHalfLetters(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void onlyBracketsHasBracketsFirst(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void onlyBracketHasOneLeftBracket(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code"));
+    }
+
+    @Test
+    public void onlyBracketHasOneLeftAndOneRight(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void onlyBracketHasOneBracketOnlyNoLetters(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
 }
